bbd0efb1a99e2b6b8271f6a94c91b296a53a5b08
bambamboo15 <11bambamboo@gmail.com>
2024-11-18 15:55:58 -0600

    commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/**
+ *    =====================================
+ *        XCC - Experimental C Compiler  
+ *    =====================================
+ * 
+ * This is a small compiler for the C language.
+ */
+#include <iostream>
+
+int main(int argc, char** argv) {}
